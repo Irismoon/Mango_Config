119895336dec8ff17b53284e858e97d157ccc24c
Mango <1121490236@qq.com>
2020-02-14 00:56:38 -0800

    add the mango-caltech computer

--- +mg/startup.m
+++ +mg/startup.m
@@ -1,10 +1,10 @@
 function def = startup(cn,un,def)
 switch upper(cn)
-    case upper('macs-MacBook.local')
+    case upper('mangos-MacBook.local')
         def.repodirs = {'hst','proc','internal','Project_Intra-Array-Exploration',...
             'MatLabCustToolbox','SeegBasic','GestureDecoding'};
         def.userdirs = {};
-    case 'MANGO_DESKTOP'
+    case 'MANGO_CALTECH'
         def.repodirs = {'hst','proc','internal','Project_Intra-Array-Exploration'};
         def.userdirrs = {};
     otherwise
