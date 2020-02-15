3387e43a916b0031a6fbeab10976a695fedd22ac
Mango <1121490236@qq.com>
2020-02-14 20:58:59 -0800

    add glia into list

--- +mg/startup.m
+++ +mg/startup.m
@@ -1,11 +1,11 @@
 function def = startup(cn,un,def)
 switch upper(cn)
-    case upper('macs-MacBook.local')
+    case upper('mangos-MacBook.local')
         def.repodirs = {'hst','proc','internal','Project_Intra-Array-Exploration',...
             'MatLabCustToolbox','SeegBasic','GestureDecoding'};
         def.userdirs = {};
-    case 'MANGO_DESKTOP'
-        def.repodirs = {'hst','proc','internal','Project_Intra-Array-Exploration'};
+    case {'MANGO_DESKTOP','GLIA.VIS.CALTECH.EDU','GLIA'}
+        def.repodirs = {'hst','proc','internal','Project_Intra-Array-Exploration','MatLabCustToolbox'};
         def.userdirrs = {};
     otherwise
         error('unexisting computer name!');
