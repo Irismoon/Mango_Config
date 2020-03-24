309629b8da5b1331398baa8d3f140e277f89d91f
Mango <1121490236@qq.com>
2020-03-24 15:49:47 +0800

    update for Mango_SJTU

---
+++ +mg/graphicSet.m
@@ -0,0 +1,11 @@
+function grasphicSet()
+set(0,'defaultAxesLineWidth',1.25);
+set(0,'defaultAxesTickLabelInterpreter','tex');
+set(0,'defaultLegendInterpreter','tex');
+set(0,'defaultTextInterpreter','tex')
+set(0,'defaultAxesOuterPosition',[0 0 1 1]);
+set(0,'defaultAxesFontName','Arial');
+set(0,'defaultAxesFontSize',11);
+set(0,'defaultlinelinewidth',2);
+end
+
